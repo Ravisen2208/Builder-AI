453d6a80759f283377bbd329c097156ab120e463
Ravisen2208 <senr19094@gmail.com>
2026-08-10 20:06:04 +0530

    AuthPage and AppContext create a sign in and sign up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,29 @@
 import React from 'react'
-import { Routes ,Route, Navigate } from 'react-router-dom'
-import { AuthLayout, GuestLayout } from './pages/Layout'
-import AuthPage from './pages/Authpage'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import { AuthLayout } from './pages/Layout'
+import AuthPage from './pages/AuthPage'
 import HomePage from './pages/Homepage'
 import BuilderPage from './pages/BuilderPage'
 import PreviewPage from './pages/PreviewPage'
 
 const App = () => {
   return (
-<Routes>
-  {/*login routes*/}
-  <Route element = {<GuestLayout/>}>
-  <Route path='/login'element= {<AuthPage mode="login"/>}/>
-  <Route path='/register'element= {<AuthPage mode="register"/>}/>
-  </Route>
+    <Routes>
 
-  {/*protected router*/}
-  <Route element = {<AuthLayout/>}>
-  <Route path='/'element= {<HomePage/>}/>
-  <Route path='/builder/:id'element= {<BuilderPage/>}/>
-  <Route path='/preview/:id'element= {<PreviewPage/>}/>
-  </Route>
-    
-    {/*Catch-all */}
-    <Route path='*' element={<Navigate to="/" replace />} />
+      {/* Login routes */}
+      <Route path="/login" element={<AuthPage mode="login" />} />
+      <Route path="/register" element={<AuthPage mode="register" />} />
 
-</Routes>
+      {/* Protected routes */}
+      <Route element={<AuthLayout />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/builder/:id" element={<BuilderPage />} />
+        <Route path="/preview/:id" element={<PreviewPage />} />
+      </Route>
+
+      <Route path="*" element={<Navigate to="/login" replace />} />
+
+    </Routes>
   )
 }
 
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,11 +1,15 @@
 import { createContext ,useContext,useEffect,useState } from "react";
 import api from "../api/api";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
 
 
 
 const AppContext = createContext(undefined);
 
 export function AppContextProvider({children}){
+
+    const navigate = useNavigate();
 
     //Auth states
      const[user , setUser] = useState(null)
@@ -16,7 +20,7 @@ export function AppContextProvider({children}){
       const checkSession = async () => {
         try{
             const{data} = await api.get("/api/auth/me")
-           // setUser(data.user);
+            setUser(data.user);
     }catch(error){
         setUser(null)
         }finally{
@@ -25,13 +29,48 @@ export function AppContextProvider({children}){
       }
       useEffect(() => {
         checkSession()
-      },[checkSession])
+      },[])
+
+      const login = async (email,password) => {
+        try{
+            const {data} = await api.post("/api/auth/login",
+                {email,password});
+                setUser(data.user)
+                toast.success("Welcome back!")
+                navigate("/")
+
+      }catch(err){
+             console.error("Login failed:", err); 
+             const errMsg = err.response?.data?.error || "Invalid email or pasword";
+                toast.error(errMsg)
+                throw new Error(errMsg)
+      }
+    }
+    
+    const register = async (name,email,password) => {
+        try{
+            const {data} = await api.post("/api/auth/register",
+                {name,email,password});
+                setUser(data.user)
+                toast.success("Account created successfully")
+                navigate("/")
+
+      }catch(err){
+             console.error("Registration failed:", err); 
+             const errMsg = err.response?.data?.error || "Registration failed";
+                toast.error(errMsg)
+                throw new Error(errMsg)
+      }
+    }
+    
 
     return(
 
         <AppContext.Provider value={{
             user,
-            loadingUser
+            loadingUser,
+            login,
+            register
         }}>
             {children}
         </AppContext.Provider>
